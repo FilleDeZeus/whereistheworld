35b935dbf2fbaf1707d8ce6252a6dfcac157c74f
FilleDeZeus <enide26@hotmail.fr>
2023-03-10 15:08:10 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+import { useRoutes } from 'react-router-dom';
 
-function App() {
+//importation des composant
+import {Donnee} from './components/donnee/donnee.js'
+import { DonneeInfo } from './components/donneeInfo/donneInfo.js';
+
+//composant App
+export const App = () => {
+
+  //les etats de ce composant
+  const [data, setData] = useState([]); //etat qui va stocké les données de l'api
+  const [selectData, setSelectData] = useState(null);//etat qui va stocké les info du donnée selectionné
+
+  //le resultat de la recherche est stocké dans data 
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setData(response.data);
+      })
+  }, []);
+  
+  //l'ensemble des routes 
+  let routes = useRoutes ([
+    {
+      path : "/",
+      element : <Donnee data={data} setSelectData={setSelectData}/>
+    }, 
+    {
+      path : "/donnee/:id",
+      element : <DonneeInfo data={selectData}  />
+    }
+    
+  ])
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      {/* affichage des routes */}
+      {routes}
+</div>
   );
-}
-
-export default App;
+};
